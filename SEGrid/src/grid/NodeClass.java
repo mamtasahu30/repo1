6ff043abe8b34b9bc37a9d44c119f5c6a0c7e0db
Aishwarya <Aishwarya@Aishwarya-PC>
2019-12-02 22:39:54 +0530

    first commit

---
+++ SEGrid/src/grid/NodeClass.java
@@ -0,0 +1,28 @@
+package grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class NodeClass {
+@Test
+@Parameters({"node","browser"})
+public void test(String n,String b) throws MalformedURLException, InterruptedException
+{
+	URL ra = new URL(n); //ip address of node system
+	DesiredCapabilities c = new DesiredCapabilities();
+	c.setBrowserName(b);//set the browser name
+	WebDriver driver = new RemoteWebDriver(ra, c);
+	driver.get("http://google.com");
+	Reporter.log(driver.getTitle(),true);
+	Thread.sleep(5000);
+	driver.close();
+	
+}
+}
